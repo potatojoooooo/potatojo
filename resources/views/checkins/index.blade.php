7290d22bcfc11962c8af1fddee2996678f02aecf
potatojoooooo <joanne0611@1utar.my>
2023-09-05 18:09:10 +0800

    added markers

--- resources/views/checkins/index.blade.php
+++ resources/views/checkins/index.blade.php
@@ -15,12 +15,13 @@
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div id="getCheckIns" data="{{ json_encode($checkIns) }}" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 @foreach($checkIns as $checkIn)
                 <div class="w-full my-4 max-w-sm bg-white rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <div class="flex px-4 pt-4">
                     </div>
                     <div class="flex flex-col items-center mb-5">
+
                         <h5 class="text-center mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ $checkIn->location }}</h5>
                         <span class="my-2 font-extrabold text-ellipsis overflow-hidden mx-10 text-sm text-gray-500 dark:text-gray-400">{{ $checkIn->check_in_notes }}</span>
                         <span class="text-ellipsis overflow-hidden mx-10 text-sm text-gray-500 dark:text-gray-400">Visited on {{ $checkIn->created_at->timezone('Asia/Kuala_Lumpur')->format('d-m-Y g:i A')  }}</span>
@@ -48,5 +49,47 @@
             </div>
 
         </div>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight pt-4 pl-2">All check-ins map</h2>
+        <div class="mt-6" id="map" style="width: 100%; height: 500px;"></div>
     </div>
+
+
+    <div class="mt-6" id="map" style="width: 100%; height: 500px;"></div>
+    <input readonly>{{auth()->user()->longitude}} </input>
+
+    <script>
+        var checkIns = document.getElementById("getCheckIns");
+        var checkIn = JSON.parse(checkIns.getAttribute("data"));
+        console.log(checkIn);
+
+        function initializeMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 10,
+                center: {
+                    lat: 3.140853,
+                    lng: 101.693207
+                }, // Initial map center
+            });
+
+            // Loop through all check-ins and add markers for each check-in
+            for (let i = 0; i < checkIn.length; i++) {
+                var latitude = checkIn[i].latitude;
+                var longitude = checkIn[i].longitude;
+                var coord = {
+                    lat: latitude,
+                    lng: longitude
+                };
+
+                new google.maps.Marker({
+                    position: coord,
+                    map: map,
+                });
+            }
+        }
+
+        // Call the initializeMap function once the Google Maps API is loaded
+        google.maps.event.addDomListener(window, 'load', initializeMap);
+    </script>
+
+
 </x-app-layout>
