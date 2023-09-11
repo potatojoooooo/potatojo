9b5d8adfbd4622ab64f091174620047265e40660
potatojoooooo <joanne0611@1utar.my>
2023-09-12 03:00:52 +0800

    search + redirect link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,18 @@
   </x-slot>
 
   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+    <div class="mb-4 w-80">
+      <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+        <input type="search" name="search" id="search" class="block w-full pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control" placeholder="Annoucements, Events, Training...">
+        <button id="searchBtn" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
+    </div>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -57,7 +69,9 @@
     let total = document.getElementById('total')
     let divResult = document.getElementById('result')
 
-
+    let q = ""
+    let searchBtn = document.getElementById('searchBtn')
+    let search = document.getElementById('search')
 
 
     function gapiLoaded() {
@@ -117,8 +131,22 @@
       }
     }
 
-    function getMessages() {
-      fetch("https://gmail.googleapis.com/gmail/v1/users/me/messages", {
+    searchBtn.onclick = () => {
+      divResult.innerHTML = "";
+      getMessages(search.value)
+    }
+
+    function getMessages(q = "") {
+
+      let url = ""
+
+      if (q == "") {
+        url = "https://gmail.googleapis.com/gmail/v1/users/me/messages"
+      } else {
+        url = `https://gmail.googleapis.com/gmail/v1/users/me/messages?q=${q}`
+      }
+
+      fetch(url, {
           method: 'GET',
           headers: new Headers({
             Authorization: `Bearer ${accessToken}`
@@ -127,6 +155,12 @@
         .then((data) => data.json())
         .then((info) => {
           console.log(info);
+
+          info.messages.sort((a, b) => {
+            const dateA = new Date(a.internalDate);
+            const dateB = new Date(b.internalDate);
+            return dateB - dateA;
+          });
 
           Array.from(info.messages).forEach((message) => {
             fetch(`https://gmail.googleapis.com/gmail/v1/users/me/messages/${message.id}`, {
@@ -152,8 +186,12 @@
                 console.log(message);
 
                 Array.from(info.payload.headers).forEach((message) => {
-                  if (message.name == "Date" || message.name == "Subject" || message.name == "From") {
-                    result.push(message.value);
+                  if (message.name == "From") {
+                    result[0] = message.value; // Push to the first position (index 0)
+                  } else if (message.name == "Date") {
+                    result[1] = message.value; // Push to the second position (index 1)
+                  } else if (message.name == "Subject") {
+                    result[2] = message.value; // Push to the third position (index 2)
                   }
                 });
 
@@ -162,11 +200,11 @@
 
                 // Display the result in the table row
                 divResult.innerHTML += `
-              <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700 event-row">
+              <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700 event-row" id='${messageData.id}'>
                 <td class="px-6 py-4">${result[0]}</td>
                 <td class="px-6 py-4">${result[1]}</td>
                 <td class="px-6 py-4">${result[2]}</td>
-                <td class="px-6 py-4">${result[3]}</td>
+                <td class="px-6 py-4"><a target="_blank" href="https://mail.google.com/mail/u/0/#inbox/${messageData.id}">${result[3]}</td>
               </tr>
             `;
               });
